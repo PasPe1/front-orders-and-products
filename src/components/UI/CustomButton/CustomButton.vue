02244f99b9e6926d81259a05e5f9504dc4097460
PasPel <pozdeev.pasha230680@gmail.com>
2024-07-28 15:55:10 +0300

    added custom UI components

---
+++ src/components/UI/CustomButton/CustomButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="button" :disabled="disabled" @click.prevent="onClick">{{text}}</button>
+</template>
+
+<script lang="ts">
+import type { PropType } from 'vue';
+
+  export default {
+    name: 'CustomButton',
+    props: {
+      text: {
+        type: String,
+        required: true
+      },
+      onClick: {
+        type: Function as PropType<(event: MouseEvent) => void>,
+        required: true
+      },
+      disabled: {
+        type: Boolean,
+        required: true
+      }
+    }
+  }
+</script>
---
+++ src/components/UI/CustomInput/CustomInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <input class="input" v-model="model" :placeholder="placeholder" />
+</template>
+
+<script setup>
+const model = defineModel()
+const { placeholder } = defineProps(['placeholder'])
+</script>
+
+<style scoped>
+.input {
+  margin: 10px;
+  background-color: #F0F3F5;
+  border-radius: 5px;
+  line-height: 24px;
+}
+</style>
